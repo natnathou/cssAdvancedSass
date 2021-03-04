acd4113dd6ae66754afc22aaeae0aac1ffe04a8d
Netanel <ne.zaffran@gmail.com>
2021-03-05 00:42:50 +0200

    style main section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Header } from './components/Header';
+import { Section } from './components/Section';
+import { Main } from './components/Main';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+export const Main = () => {
+  return (
+    <main>
+      <section className='section-about'>
+        <div className='u-center-text u-margin-bottom-big'>
+          <h2 className='heading-secondary'>
+            Exciting tours for adventurous people
+          </h2>
+        </div>
+
+        <div className='row'>
+          <div className='col-1-of-2'>
+            <h3 className='heading-tertiary u-margin-bottom-small'>
+              You're going to fall in love with nature
+            </h3>
+            <p className='paragraph'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+              ipsum sapiente aspernatur libero repellat quis consequatur ducimus
+              quam nisi exercitationem omnis earum qui.
+            </p>
+
+            <h3 className='heading-tertiary u-margin-bottom-small'>
+              Live adventures like you never have before
+            </h3>
+            <p className='paragraph'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores nulla deserunt voluptatum nam.
+            </p>
+
+            <a href='#' className='btn-text'>
+              Learn more &rarr;
+            </a>
+          </div>
+          <div className='col-1-of-2'>
+            {/* <div className='composition'>
+              <img
+                src='img/nat-1-large.jpg'
+                alt='Photo 1'
+                className='composition__photo composition__photo--p1'
+              />
+              <img
+                src='img/nat-2-large.jpg'
+                alt='Photo 2'
+                className='composition__photo composition__photo--p2'
+              />
+              <img
+                src='img/nat-3-large.jpg'
+                alt='Photo 3'
+                className='composition__photo composition__photo--p3'
+              />
+            </div> */}
+            ooooo
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
